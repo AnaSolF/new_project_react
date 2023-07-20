4e5ec0dc60cfc4b7d47e48a68473e061160ae6bc
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-07-20 17:52:15 -0300

    Trabajamos con contexto

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,14 +7,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import styles from "../styles/header.module.css"
+import { BsFillCartPlusFill } from "react-icons/bs";
+import { useContext } from 'react';
+import { MainContext } from '@/Context/MainContextProvider'
 
 const Header = () => {
+    const defaultState = useContext(MainContext);
     return (
             <>
                 {['lg'].map((expand) => (
                     <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
                         <Container fluid>
-                            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+                            <Navbar.Brand href="#">Pedí Ya</Navbar.Brand>
                             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
                             <Navbar.Offcanvas
                                 id={`offcanvasNavbar-expand-${expand}`}
@@ -23,36 +27,16 @@ const Header = () => {
                             >
                                 <Offcanvas.Header closeButton>
                                     <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                                        Offcanvas
+                                        Pedi Ya
                                     </Offcanvas.Title>
                                 </Offcanvas.Header>
                                 <Offcanvas.Body>
                                     <Nav className="justify-content-end flex-grow-1 pe-3">
-                                        <Nav.Link href="#action1">Home</Nav.Link>
-                                        <Nav.Link href="#action2">Link</Nav.Link>
-                                        <NavDropdown
-                                            title="Dropdown"
-                                            id={`offcanvasNavbarDropdown-expand-${expand}`}
-                                        >
-                                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action4">
-                                                Another action
-                                            </NavDropdown.Item>
-                                            <NavDropdown.Divider />
-                                            <NavDropdown.Item href="#action5">
-                                                Something else here
-                                            </NavDropdown.Item>
-                                        </NavDropdown>
+                                        <Nav.Link href="#action1">Ingresar</Nav.Link>
+                                        <Nav.Link href="#action2"><BsFillCartPlusFill /></Nav.Link>
+                                       
                                     </Nav>
-                                    <Form className="d-flex">
-                                        <Form.Control
-                                            type="search"
-                                            placeholder="Search"
-                                            className="me-2"
-                                            aria-label="Search"
-                                        />
-                                        <Button variant="outline-success">Search</Button>
-                                    </Form>
+                                  
                                 </Offcanvas.Body>
                             </Navbar.Offcanvas>
                         </Container>
---
+++ src/pages/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>Cart</div>
+  )
+}
+
+export default Cart
+
+//Necesita recibir algunas variables y funciones del contexto inicial y setearlas
+//isLoggedIn
+//userName
+//Ver Cart 
